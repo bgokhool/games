335ea56a16c99e73c54f4875d76314886cce6bf0
Bhuwan Gokhool <gokhoolb.union.edu>
2020-07-02 17:06:49 -0400

    Add Stack class for Breadth First Search

--- sudoku/search_problem.py
+++ sudoku/search_problem.py
@@ -32,6 +32,38 @@ class Queue:
     def empty(self):
         return len(self.queue) == 0
 
+
+class Stack:
+    """
+    A Stack class to be used in combination with state space
+    search for backtracking search.
+    """
+    def __init__(self):
+        self.stack = []
+
+    def __str__(self):
+        result = "Stack contains " + str(len(self.queue)) + " items\n"
+        for item in self.stack:
+            result += str(item) + "\n"
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        toReturn = self.stack[-1]
+        del self.stack[-1]
+        return toReturn
+
+    def top(self):
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+    def empty(self):
+        return self.size() == 0
+
+
 class Node:
     """
     A Node class to be used in combination with state space search.  A
@@ -42,7 +74,7 @@ class Node:
         self.state = state
         self.parent = parent
         self.depth = depth
-        
+
     def __str__(self):
         result = "\nState: " +  str(self.state)
         result += "\nDepth: " + str(self.depth)
@@ -52,52 +84,55 @@ class Node:
 
 class Search:
     """
-    A general Search class that can be used for any problem domain.
-    Given instances of an initial state and a goal state in the
-    problem domain, this class will print the solution or a failure
-    message.  The problem domain should be based on the ProblemState
+    A Search class that can be used for the Sudoku problem domain.
+    Given instances of an initial state, this class will return the
+    number of possible solutions by using backtracking search.
+    The problem domain should be based on the ProblemState
     class.
     """
-    def __init__(self, initialState, goalState, verbose=False):
+    total_solns = 0
+
+    def __init__(self, initialState, verbose=False):
         self.uniqueStates = {}
         self.uniqueStates[initialState.dictkey()] = True
-        self.q = Queue()
-        self.q.enqueue(Node(initialState, None, 0))
-        self.goalState = goalState
+        self.q = Stack()
+        self.q.push(Node(initialState, None, 0))
+
         self.verbose = verbose
         solution = self.execute()
-        if solution == None:
-            print( "Search failed")
-        else:
-            self.showPath(solution)
+
+    def getNumSolns(self):
+        """
+        Returns total number of solutions found
+        """
+        return self.total_solns
+
+
     def execute(self):
         while not self.q.empty():
-            current = self.q.dequeue()
-            if self.goalState.equals(current.state):
-                return current
+            current = self.q.pop()
+            if current.state.isDone():
+                self.total_solns += 1
             else:
-                successors = current.state.applyOperators()
+                successors = current.state.followingStates()
                 for nextState in successors:
-                    #if not self.uniqueStates.has_key(nextState.dictkey()):
-                    # simple <key> in <dictionary> works too, but I find it
-                    # more semantically vague than
-                    # <key> in <dictionary>.keys()
                     if nextState.dictkey() not in self.uniqueStates.keys():
                         n = Node(nextState, current, current.depth+1)
-                        self.q.enqueue(n)
+                        self.q.push(n)
                         self.uniqueStates[nextState.dictkey()] = True
                 if self.verbose:
-                    print( "Expanded:", current)
-                    print( "Number of successors:", len(successors))
+                    print("Expanded:", current)
+                    print("Number of successors:", len(successors))
                     print("Queue length:", self.q.size())
                     print( "-------------------------------")
 
-        return None
+
     def showPath(self, node):
         path = self.buildPath(node)
         for current in path:
             print( current.state)
         print("Goal reached in", current.depth, "steps")
+
     def buildPath(self, node):
         """
         Beginning at the goal node, follow the parent links back
@@ -138,3 +173,28 @@ class ProblemState:
         represent unique states.
         """
         abstract()
+
+
+import sudoku
+import generator
+
+if __name__ == "__main__":
+    full_board = [
+        [ 3, 4, 8, 7, 1, 2, 9, 6, 5],
+        [ 2, 5, 6, 9, 4, 8, 7, 1, 3],
+        [ 9, 7, 1, 5, 6, 3, 4, 8, 2],
+        [ 4, 9, 7, 1, 3, 5, 6, 2, 8],
+        [ 5, 6, 3, 8, 2, 7, 1, 9, 4],
+        [ 8, 1, 2, 4, 9, 6, 3, 5, 7],
+        [ 7, 3, 9, 2, 8, 1, 5, 4, 6],
+        [ 6, 2, 4, 3, 5, 9, 8, 7, 1],
+        [ 1, 8, 5, 6, 7, 4, 2, 3, 9]
+    ]
+
+    for i in range(9):
+        full_board[i%3][i] = 0
+        full_board[5 - i%3][i] = 0
+        full_board[6 + i%3][i] = 0
+    game = sudoku.Sudoku(full_board)
+    game.printBoard()
+    print(Search(game).getNumSolns())
